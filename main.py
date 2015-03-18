dc804cd24531333a15ca6dc1ec560c4161bb929b
northcu2 <northcu2@illinois.edu>
2015-03-18 16:51:55 -0500

    Working with user auth

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 
-from flask import Flask, render_template, redirect, url_for, request
-
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wrap
 app = Flask(__name__)
 
+app.secret_key = "not a secure key"
+
+def login_required(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('You need to login first!')
+			return refirect(url_for('login'))
+	return wrap
+
+ 
+
 @app.route('/')
+@login_required
 def home():
-    return "Hello, World!"
+   return render_template('index.html')
 
 @app.route('/welcome')
 def welcome():
@@ -18,8 +33,15 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('welcome'))
+			session['logged_in'] = True
+			flash('You Succesfully logged in!')	
+            return redirect(url_for('home'))
     return render_template('login.html', error=error)
-
+@app.route('/logout')
+@login_required
+def logout():
+	session.pop('logged_in', None)
+	flash('You Succesfully logged out!')	
+	return refirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True)
