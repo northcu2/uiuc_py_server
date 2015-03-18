1cd3b327bd0c338ae044898fcdc419816258115a
northcu2 <northcu2@illinois.edu>
2015-03-18 15:56:14 -0500

    Debugging

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for, request
+
+from flask import Flask, render_template, redirect, url_for, request
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Hello, World!"
@@ -7,15 +10,16 @@ def home():
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+	
 @app.route('/login', methods=['GET', 'POST'])
-def login()
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'Eddie' or request.form['username'] != 'Jasper' or request.form['password'] != 'admin':
-			error = "Invalid User Credentials. Please try again."
-		else:
-			return redirect(url_for('welcome"))
-	return render_template('login.html', error=error)
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
