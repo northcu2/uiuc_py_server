41461e8e06481507b6674708a0be050075e6f5c7
northcu2 <northcu2@illinois.edu>
2015-03-18 14:55:12 -0500

    Fixed HTML Doc and Python code

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-# import the Flask class from the flask module
 from flask import Flask, render_template
-
-# create the application object
 app = Flask(__name__)
-
-# use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Hello, World!"  # return a string
+    return "Hello, World!"
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html')  # render a template
+    return render_template('welcome.html')
 
-# start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
