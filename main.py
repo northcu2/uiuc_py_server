9e4b1f50885af8ccf5ac92964d732a6a2f27a009
northcu2 <northcu2@illinois.edu>
2015-03-18 15:38:37 -0500

    Log in page Test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -7,6 +7,15 @@ def home():
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login()
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'Eddie' or request.form['username'] != 'Jasper' or request.form['password'] != 'admin':
+			error = "Invalid User Credentials. Please try again."
+		else:
+			return redirect(url_for('welcome"))
+	return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
