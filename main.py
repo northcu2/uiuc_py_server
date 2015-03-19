5cb3685eb0e0568bab0d4efd0e9fb595b48ef1aa
northcu2 <northcu2@illinois.edu>
2015-03-19 11:29:37 -0500

    ...

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-
+# import the Flask class from the flask module
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from functools import wraps
+
+# create the application object
 app = Flask(__name__)
 
-app.secret_key = 'not a secure key'
+# config
+app.secret_key = 'my precious'
 
+# login required decorator
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -15,17 +19,18 @@ def login_required(f):
             return redirect(url_for('login'))
     return wrap
 
- 
-
+# use decorators to link the function to a url
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.html')
+    return render_template('index.html')  # render a template
+    # return "Hello, World!"  # return a string
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html'
-	
+    return render_template('welcome.html')  # render a template
+
+# route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -38,13 +43,14 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
-	
-	
 @app.route('/logout')
 @login_required
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out.')
     return redirect(url_for('welcome'))
+
+
+# start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
