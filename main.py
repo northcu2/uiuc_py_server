f4cbe4cbaf0c6bd870abd2a6e0ce137d53f01e05
northcu2 <northcu2@illinois.edu>
2015-03-19 14:31:38 -0700

    Added Database

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from functools import wraps
+import sqlite3
 
-# create the application object
+
 app = Flask(__name__)
 
-# config
-app.secret_key = 'my precious'
 
-# login required decorator
+
+app.secret_key = 'somthing'
+
+app.database = "sample.db"
+
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -19,18 +22,24 @@ def login_required(f):
             return redirect(url_for('login'))
     return wrap
 
-# use decorators to link the function to a url
+
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.html')  # render a template
     # return "Hello, World!"  # return a string
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html', posts=posts)
+
+
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html')  # render a template
+    return render_template('welcome.html')  
 
-# route for handling the login page logic
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -50,7 +59,10 @@ def logout():
     flash('You were logged out.')
     return redirect(url_for('welcome'))
 
+def connect_db():
+    return sqlite3.connect(app.database)
 
-# start the server with the 'run()' method
+
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ sql.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+with sqlite3.connect("sample.db") as connection:
+	c = connection.cursor()
+	c.execute('CREATE TABLE posts(title TEXT, details TEXT)')
+	c.execute('INSERT INTO posts VALUES("This database", "is working.")')
+	c.execute('INSERT INTO posts VALUES("This database", "is awesome.")')
