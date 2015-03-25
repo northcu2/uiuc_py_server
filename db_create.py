7b281e5837b432299a24275bf792720e44785d8b
northcu2 <northcu2@illinois.edu>
2015-03-25 17:54:02 -0500

    Working out bugs with heroku

--- db_create.py
+++ db_create.py
@@ -1,10 +1,14 @@
-from main import db
+from app import db
 from models import BlogPost
 
+# create the database and the db table
 db.create_all()
 
-db.session.add(BlogPost("Good","This SQLALC post is good"))
-db.session.add(BlogPost("better","This SQLALC post is better"))
+# insert data
+db.session.add(BlogPost("Good", "I\'m good."))
+db.session.add(BlogPost("Well", "I\'m well."))
+db.session.add(BlogPost("Excellent", "I\'m excellent."))
+db.session.add(BlogPost("Okay", "I\'m okay."))
 
-
+# commit the changes
 db.session.commit()
